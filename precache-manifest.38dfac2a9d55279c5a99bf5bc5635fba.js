b7d69de00a22339849ff03e3a8fa3b76ddd82b54
burakozkan <burakozkan.ceng@gmail.com>
2019-07-30 11:36:25 +0300

    Updates

---
+++ precache-manifest.38dfac2a9d55279c5a99bf5bc5635fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1bec5d2e2a44ca406a5155529e31dc",
+    "url": "/hellodash/index.html"
+  },
+  {
+    "revision": "5571f56fdfe0e1244f6b",
+    "url": "/hellodash/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hellodash/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5571f56fdfe0e1244f6b",
+    "url": "/hellodash/static/js/main.c9447a12.chunk.js"
+  },
+  {
+    "revision": "e7d0b37119cd8f749284",
+    "url": "/hellodash/static/js/runtime~main.e72c6626.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hellodash/static/media/logo.5d5d9eef.svg"
+  }
+]);
